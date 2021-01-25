423875595137dbfcc99689b8b77e751c9ef3639c
Стас <stasmq@gmail.com>
2021-01-25 12:26:53 +0200

    bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularVlaMin';
+
+  title = 'Dynamic title';
+  number = 42;
+  arr = [1, 2, 3];
+  obj = {a: 1, b: {c: 2}};
+  img = 'https://seeklogo.com/images/R/react-logo-7B3CE81517-seeklogo.com.png';
+
+  constructor() {
+  setTimeout(() => {
+    console.log('Timeout is over');
+    this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png';
+  }, 5000);
+  }
 }
