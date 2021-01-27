41d261767b7874d72f2ce6e706ec39d3bb997754
Стас <stasmq@gmail.com>
2021-01-27 05:34:30 +0200

    Way Binding part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,34 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  title = 'Dynamic title';
-  number = 42;
-  arr = [1, 2, 3];
-  obj = {a: 1, b: {c: 2}};
+  title = 'Initial';
 
-  imputValue = '';
-
-  // img = 'https://seeklogo.com/images/R/react-logo-7B3CE81517-seeklogo.com.png';
-
-  constructor() {
-  // setTimeout(() => {
-  //   console.log('Timeout is over');
-  //   this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png';
-  // }, 5000);
+  onInput(event: any){
+    this.title = event.target.value;
   }
-
-  onInput(event: KeyboardEvent){
-    this.imputValue = (<HTMLInputElement>event.target).value
-  }
-
-  onBlur(str: string){
-      this.imputValue = str;
-  }
-
-  onClick(){
-    console.log('Click!');
-  }
-
 }
 
 
