28218301c34f51c2e98df0cec6493e318abb5f99
Стас <stasmq@gmail.com>
2021-01-27 07:33:16 +0200

    Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
- arr = [1, 1, 2, 3, 5, 8, 13 ];
-
- objs = [
-   {title: 'Post1', author: 'Stas', comments: [
-       {name: 'Max', text: 'lorem1'},
-       {name: 'Max', text: 'lorem2'},
-       {name: 'Max', text: 'lorem3'}
-     ]},
-   {title: 'Post2', author: 'Stas2', comments: [
-       {name: 'Max2', text: 'lorem1'},
-       {name: 'Max2', text: 'lorem2'},
-       {name: 'Max2', text: 'lorem3'}
-     ]},
-   ];
-
+now: Date = new Date()
 }
 
 
