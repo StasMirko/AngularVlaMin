a547284115a728bec3d610c1bda381b355aeb126
Стас <stasmq@gmail.com>
2021-01-27 10:11:16 +0200

    Component 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+posts: Post[] = [
+  {title: 'I want to learn Angular component', text: 'I learn component', id: 1},
+  {title: 'The next block', text: 'It will be about directives and about pipes yet', id: 2}
+]
 }
 
 
