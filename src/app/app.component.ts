48888c0af4fa3aa8cc67ef4ec847febd5c1cb38a
Стас <stasmq@gmail.com>
2021-01-27 07:24:40 +0200

    Директива ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  toggle: any = false;
+ arr = [1, 1, 2, 3, 5, 8, 13 ];
+
+ objs = [
+   {title: 'Post1', author: 'Stas', comments: [
+       {name: 'Max', text: 'lorem1'},
+       {name: 'Max', text: 'lorem2'},
+       {name: 'Max', text: 'lorem3'}
+     ]},
+   {title: 'Post2', author: 'Stas2', comments: [
+       {name: 'Max2', text: 'lorem1'},
+       {name: 'Max2', text: 'lorem2'},
+       {name: 'Max2', text: 'lorem3'}
+     ]},
+   ];
 
 }
 
